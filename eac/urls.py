79b171954ac64a16673b727d823488dbaacb6b47
AnnKpx Server <annkpx@gmail.com>
2013-02-04 02:04:30 +0400

    settings

--- eac/urls.py
+++ eac/urls.py
@@ -26,8 +26,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),   
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
+urlpatterns += patterns('',
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+)
